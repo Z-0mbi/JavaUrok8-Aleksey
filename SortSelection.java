a270360823cd9cd37dac52d7f6d6252676448b24
Alex Strakh <zetobor@gmail.com>
2017-09-07 14:26:59 +0300

    Urok8

---
+++ SortSelection.java
@@ -0,0 +1,34 @@
+package sortSelection;
+
+public class SortSelection {
+	
+	public static void makeSortSelection(int[] mas) {
+		int k = mas[0];//временная переменная
+		int temp;//Позицие элемента меньшего, чем темп
+		for (int j=0; j<mas.length-1;j++) {
+			k = mas[j];
+			for (int i=j+1; i<mas.length; i++) {
+				if (mas[i] < k) {
+					temp = k;
+					k = mas[i];
+					mas[i] = temp;
+				}
+			}
+			mas[j] = k;//Присваиваем наименьший элемент 
+			
+		}
+		
+	}
+	
+	public static void showMas(int[] mas) {
+		for (int i=0; i<mas.length; i++) {
+			System.out.print(mas[i] + " ");
+		}
+	}
+	public static void main(String[] args) {
+		int[] mas = {2,5,3,456,4,89,6,456,6,47,78};
+		SortSelection.makeSortSelection(mas);
+		SortSelection.showMas(mas);
+	}
+
+}
